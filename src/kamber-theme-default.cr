97ce970d704168a28654d16221e2da21cd3b8d61
Fatih Kadir Akın <fakin@protel.com.tr>
2015-12-08 21:50:27 +0200

    update

---
+++ src/kamber-theme-default.cr
@@ -0,0 +1,15 @@
+def theme_style(path)
+  "libs/kamber-theme-default/static/css/#{path}"
+end
+
+def theme_script(path)
+  "libs/kamber-theme-default/static/js/#{path}"
+end
+
+def theme_item
+  render "libs/kamber-theme-default/views/post.ecr"
+end
+
+def theme_index
+  render "libs/kamber-theme-default/views/index.ecr"
+end
